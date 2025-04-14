cc3b49c0c1fd2a6771324bdd14d68a6f93ae889e
surabhiwaingankar <surabhiwaingankar@gmail.com>
2025-04-14 23:57:25 +0530

    Refactor skill gap analysis endpoint to return missing skills from resume compared to job description

--- ml/skill_gap_analysis/skill_gap_router.py
+++ ml/skill_gap_analysis/skill_gap_router.py
@@ -7,13 +7,20 @@ router = APIRouter(tags=["skill-gap-analysis"])
 
 
 @router.post("/skill-gap-analysis")
-async def analyze(
-    resume: UploadFile = File(...),
-    job_description: str = Form(...)):
-    contents = await resume.read()
-    
-    skills = parse_resume(contents)
-    
-    return JSONResponse(content={
-        "skills": skills
-    })
+async def analyze(resume: UploadFile = File(...), job_description: UploadFile = File(...)):
+    resume = await resume.read()
+    job_description = await job_description.read()
+
+    resume_skills = parse_resume(resume)
+    jd_skills = parse_resume(job_description)
+
+    print("Resume Skills:", resume_skills)
+    print("Job Description Skills:", jd_skills)
+
+    missing_skills = set(jd_skills) - set(resume_skills)
+
+    return JSONResponse(
+        content={
+            "missing_skills": list(missing_skills),
+        }
+    )
