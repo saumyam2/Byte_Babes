a38470dda8c788f1f50a939df1b5056ac6d38f68
Saumya Mehta <saumyakmehta26@gmail.com>
2025-05-07 00:40:06 +0530

    Features added to intent classifier

--- ml/intent_classifier/intent_client.py
+++ ml/intent_classifier/intent_client.py
@@ -13,7 +13,7 @@ def classify_intent(user_message: str) -> str:
     prompt = f"""
     Classify the intent of the following user message into one of these categories:
     ["cold_email", "career_roadmap", "job_search", "mentor_match", "skill_gap_analysis", 
-    "success_stories", "resume_feedback", "find_event", "general"]
+    "success_stories", "resume_feedback", "find_event", "general", "cover_letter", "referral"]
 
     User message: "{user_message}"
     Only return the intent label.
